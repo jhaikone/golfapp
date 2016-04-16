2280628c92de20a2014fe191b82ba52f1cee3ceb
Juuso <juusohaikonen@hotmail.com>
2016-04-16 15:44:34 +0300

    added about and fixed navigation

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -1,12 +1,21 @@
 export function routerConfig ($stateProvider, $urlRouterProvider) {
   'ngInject';
   $stateProvider
-    .state('home', {
-      url: '/',
+    .state('navbar', {
+        abstract: true,
+        template: '<div><header><acme-navbar creation-date="main.creationDate"></acme-navbar></header><ui-view></ui-view></div>'
+    })
+    .state('navbar.home', {
+      url: '/home',
       templateUrl: 'app/main/main.html',
       controller: 'MainController',
       controllerAs: 'main'
-    });
+    })
+    .state('navbar.about', {
+      url: '/about',
+      template: '<div>about</div>'
+    })
+    ;
 
   $urlRouterProvider.otherwise('/');
 }
